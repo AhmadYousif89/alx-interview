9e63a8ee1982eb8bd376c90f91f8ab8da0f78bd6
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-07-25 15:24:59 +0300

    Refactor pascal_triangle function to include a docstring describing its purpose

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""
-Implement the Pascal’s triangle algorithm
-"""
+"""Implement the Pascal’s triangle algorithm"""
 
 
 def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal’s triangle of n
+    """
     triangle = []
     for row in range(n):
         triangle.append([1] * (row + 1))
