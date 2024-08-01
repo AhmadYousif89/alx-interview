9cab2c11419226e8a3a60e0b4e85a0c0b9177c05
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-08-01 20:48:17 +0300

    Remove test cases

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -21,14 +21,3 @@ def canUnlockAll(boxes):
         keys = new_keys
 
     return len(opened) == n
-
-
-# Example usage:
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))  # True
-
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))  # True
-
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))  # False
