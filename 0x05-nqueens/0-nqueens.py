716884516d1401444064232a07ee60b43b82a28f
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-08-29 20:23:14 +0300

    Refactor nqueens_puzzle function and solve_puzzle function

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -42,58 +42,56 @@ def nqueens_puzzle(N):
     where `r` and `c` represent the row and column, respectively, where a
     queen must be placed on the chessboard.
     """
-
     # Initialize the chess board & the output results list matrix
     results = []
     board = [-1] * N
 
-    # solve_puzzle(board, 0, N, results)
+    solve_puzzle(board, 0, N, results)
 
     for solution in results:
         print(solution)
 
 
-# def solve_puzzle(board, row, N, results):
-#     """
-#     Solves the N-Queens puzzle recursively.
-#     Args:
-#     - board (list): The current working chessboard.
-#     - row (int): The queen's current row position.
-#     - N (int): The number of queens.
-#     - results (list): Matrix containing all the chessboards.
-#     Returns:
-#     - list: Updated results matrix.
-#     """
+def solve_puzzle(board, row, N, results):
+    """
+    Solves the N-Queens puzzle recursively.
 
-
-#     # Base case: all queens are placed
-#     if row == N:
-#         # add the current solution to the results
-#         results.append([[i, board[i]] for i in range(N)])
-#         return
-#     # Try placing a queen in each row of the current column
-#     for col in range(N):
-#         if is_safe(board, row, col):
-#             board[row] = col
-#             solve_puzzle(board, row + 1, N, results)
+    Args :
+    - board (list): The current working chessboard.
+    - row (int): The queen's current row position.
+    - N (int): The number of queens.
+    - results (list): Matrix containing all the chessboards.
+    """
+    # Base case: all queens are placed
+    if row == N:
+        # add the current solution to the results
+        results.append([[i, board[i]] for i in range(N)])
+        return
+    # Try placing a queen in each row of the current column
+    for col in range(N):
+        if is_safe(board, row, col):
+            board[row] = col
+            solve_puzzle(board, row + 1, N, results)
 
 
 def is_safe(board, row, col):
     """
     Helper function that checks the board before placing a queen.
-    Args:
+
+    Args :
     - board (list): The current working chessboard.
     - row (int): The queen last position on a row.
     - col (int): The queen last position on a col.
-    Return:
-    - True if it's safe to place the queen or False otherwise.
+
+    Returns :
+        True if it's safe to place the queen or False otherwise.
     """
     # Check if there is a queen in the same column
     for i in range(row):
         if (
-            board[i] == col  # queen vertically in the same column
-            or board[i] + i == col + row  # queen in the right diagonal
-            or board[i] - i == col - row  # queen in the left diagonal
+            board[i] == col or # queen vertically in the same column
+            board[i] + i == col + row or # queen in the right diagonal
+            board[i] - i == col - row # queen in the left diagonal
         ):
             return False
     return True
