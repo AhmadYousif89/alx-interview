a603c5cc8a14ecebeffb9053710f1b0f8441091b
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-03 18:11:32 +0300

    Update docs and fix styleing error

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 /**
  * script that prints all characters of a Star Wars movie
- * Usage: ./0-starwars_characters.js [movieId]
+ * Usage: ./0-starwars_characters.js [film_id]
  *
  * Description:
- * * Fetch a starwars movie based on the passed ID and display one character name per line
- * * in the same order as the “characters” list in the /films/ endpoint
+ * * Fetch a starwars movie based on the passed ID and display one character name
+ * * per line in the same order as the “characters” list in the /films/ endpoint
  */
 
 const request = require('request');
@@ -13,11 +13,11 @@ const util = require('util');
 
 const requestPromise = util.promisify(request);
 
-const url = `https://swapi-api.alx-tools.com/api/films/`;
+const url = 'https://swapi-api.alx-tools.com/api/films/';
 
-async function getStarwarsCharacters(id) {
+async function getStarwarsCharacters (id) {
   if (!id || isNaN(+id) || +id < 1) {
-    console.log('Usage: ./0-starwars_characters.js [movieId]');
+    console.log('Usage: ./0-starwars_characters.js [film_id]');
     return;
   }
 
